4879386755d1e0aeed1ad830dfe492093107d08e
TaiNM <tainm@foodconnection.vn>
2026-05-11 17:48:44 +0700

    update code

--- shared/js/test.js
+++ shared/js/test.js
@@ -4,3 +4,10 @@ document.cookie =
     "from_test2_179088=123123123; path=/; expires=Fri, 31 Dec 2026 23:59:59 GMT";
 document.cookie =
     "from_test3_178330=123123123; path=/; expires=Fri, 31 Dec 2026 23:59:59 GMT";
+
+document.cookie =
+    "from_test4=123123123; path=/; expires=Fri, 31 Dec 2026 23:59:59 GMT";
+document.cookie =
+    "from_test5_179088=123123123; path=/; expires=Fri, 31 Dec 2026 23:59:59 GMT";
+document.cookie =
+    "from_test6_178330=123123123; path=/; expires=Fri, 31 Dec 2026 23:59:59 GMT";
